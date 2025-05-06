9e60102f9571d424b83d4d6022c03418ecbb1c6c
Hugo Schweizer <hugschweize@etmlnet.local>
2025-05-06 09:09:46 +0200

    ajout du fond écran

--- Idle Game --Strange Village--/Form1.Designer.cs
+++ Idle Game --Strange Village--/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.background1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // background1
+            // 
+            this.background1.BackgroundImage = global::Idle_Game___Strange_Village__.Properties.Resources.Capture;
+            this.background1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.background1.Location = new System.Drawing.Point(-1, -1);
+            this.background1.Name = "background1";
+            this.background1.Size = new System.Drawing.Size(804, 455);
+            this.background1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.background1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel background1;
     }
 }
 
